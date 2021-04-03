e2d93e9ddd9f22bae6bedc0d2a39dd1c578af539
Joan Andrés <andres6936@live.com>
2021-04-02 21:24:17 -0500

    Fixed include

--- Include/Httplib/Httplib.hpp
+++ Include/Httplib/Httplib.hpp
@@ -116,27 +116,6 @@ inline std::string if2ip(const std::string &ifn) {
 
 
 
-
-
-inline constexpr unsigned int str2tag_core(const char *s, size_t l,
-                                           unsigned int h) {
-  return (l == 0) ? h
-                  : str2tag_core(s + 1, l - 1,
-                                 (h * 33) ^ static_cast<unsigned char>(*s));
-}
-
-inline unsigned int str2tag(const std::string &s) {
-  return str2tag_core(s.data(), s.size(), 0);
-}
-
-namespace udl {
-
-inline constexpr unsigned int operator"" _(const char *s, size_t l) {
-  return str2tag_core(s, l, 0);
-}
-
-} // namespace udl
-
 #ifdef CPPHTTPLIB_BROTLI_SUPPORT
 class brotli_compressor : public compressor {
 public:
