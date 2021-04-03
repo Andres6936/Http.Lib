7d598d7538a23dfeb9d52d09a77cf6f00ebe7757
Joan Andrés <andres6936@live.com>
2021-04-02 21:51:00 -0500

    Avoid problem with template function

--- Include/Httplib/Stream.hpp
+++ Include/Httplib/Stream.hpp
@@ -3,7 +3,9 @@
 #ifndef HTTPLIB_STREAM_HPP
 #define HTTPLIB_STREAM_HPP
 
+#include <array>
 #include <string>
+#include <vector>
 
 #include <Httplib/Using/SocketType.hpp>
 
@@ -22,7 +24,40 @@ public:
   virtual socket_t socket() const = 0;
 
   template <typename... Args>
-  ssize_t write_format(const char *fmt, const Args &... args);
+  ssize_t write_format(const char *fmt, const Args &... args)
+  {
+    const auto bufsiz = 2048;
+    std::array<char, bufsiz> buf;
+
+#if defined(_MSC_VER) && _MSC_VER < 1900
+    auto sn = _snprintf_s(buf.data(), bufsiz - 1, buf.size() - 1, fmt, args...);
+#else
+    auto sn = snprintf(buf.data(), buf.size() - 1, fmt, args...);
+#endif
+    if (sn <= 0) { return sn; }
+
+    auto n = static_cast<size_t>(sn);
+
+    if (n >= buf.size() - 1) {
+      std::vector<char> glowable_buf(buf.size());
+
+      while (n >= glowable_buf.size() - 1) {
+        glowable_buf.resize(glowable_buf.size() * 2);
+#if defined(_MSC_VER) && _MSC_VER < 1900
+        n = static_cast<size_t>(_snprintf_s(&glowable_buf[0], glowable_buf.size(),
+                                          glowable_buf.size() - 1, fmt,
+                                          args...));
+#else
+        n = static_cast<size_t>(
+            snprintf(&glowable_buf[0], glowable_buf.size() - 1, fmt, args...));
+#endif
+      }
+      return write(&glowable_buf[0], n);
+    } else {
+      return write(buf.data(), n);
+    }
+  }
+
   ssize_t write(const char *ptr);
   ssize_t write(const std::string &s);
 };
--- Source/Stream.cpp
+++ Source/Stream.cpp
@@ -17,37 +17,3 @@ ssize_t Stream::write(const char *ptr) {
 ssize_t Stream::write(const std::string &s) {
   return write(s.data(), s.size());
 }
-
-template <typename... Args>
-ssize_t Stream::write_format(const char *fmt, const Args &... args) {
-  const auto bufsiz = 2048;
-  std::array<char, bufsiz> buf;
-
-#if defined(_MSC_VER) && _MSC_VER < 1900
-  auto sn = _snprintf_s(buf.data(), bufsiz - 1, buf.size() - 1, fmt, args...);
-#else
-  auto sn = snprintf(buf.data(), buf.size() - 1, fmt, args...);
-#endif
-  if (sn <= 0) { return sn; }
-
-  auto n = static_cast<size_t>(sn);
-
-  if (n >= buf.size() - 1) {
-    std::vector<char> glowable_buf(buf.size());
-
-    while (n >= glowable_buf.size() - 1) {
-      glowable_buf.resize(glowable_buf.size() * 2);
-#if defined(_MSC_VER) && _MSC_VER < 1900
-      n = static_cast<size_t>(_snprintf_s(&glowable_buf[0], glowable_buf.size(),
-                                          glowable_buf.size() - 1, fmt,
-                                          args...));
-#else
-      n = static_cast<size_t>(
-          snprintf(&glowable_buf[0], glowable_buf.size() - 1, fmt, args...));
-#endif
-    }
-    return write(&glowable_buf[0], n);
-  } else {
-    return write(buf.data(), n);
-  }
-}
