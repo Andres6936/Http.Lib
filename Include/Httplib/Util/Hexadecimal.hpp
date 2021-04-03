bc2abd98d684039403c5f48b3061514e23acbf3e
Joan Andrés <andres6936@live.com>
2021-04-02 20:26:05 -0500

    Divided sources

---
+++ Include/Httplib/Util/Hexadecimal.hpp
@@ -0,0 +1,56 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef HTTPLIB_HEXADECIMAL_HPP
+#define HTTPLIB_HEXADECIMAL_HPP
+
+namespace httplib {
+
+namespace detail {
+
+
+inline bool is_hex(char c, int &v) {
+  if (0x20 <= c && isdigit(c)) {
+    v = c - '0';
+    return true;
+  } else if ('A' <= c && c <= 'F') {
+    v = c - 'A' + 10;
+    return true;
+  } else if ('a' <= c && c <= 'f') {
+    v = c - 'a' + 10;
+    return true;
+  }
+  return false;
+}
+
+inline bool from_hex_to_i(const std::string &s, size_t i, size_t cnt,
+    int &val) {
+  if (i >= s.size()) { return false; }
+
+  val = 0;
+  for (; cnt; i++, cnt--) {
+    if (!s[i]) { return false; }
+    int v = 0;
+    if (is_hex(s[i], v)) {
+      val = val * 16 + v;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+inline std::string from_i_to_hex(size_t n) {
+  const char *charset = "0123456789abcdef";
+  std::string ret;
+  do {
+    ret = charset[n & 15] + ret;
+    n >>= 4;
+  } while (n > 0);
+  return ret;
+}
+
+}
+
+} // namespace httplib
+
+#endif // HTTPLIB_HEXADECIMAL_HPP
