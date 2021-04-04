37591e5de82d9c7c4c797804395aea12bd68dba4
Joan Andrés <andres6936@live.com>
2021-04-03 23:02:55 -0500

    Executed Test with GTest

--- test/test.cc
+++ test/test.cc
@@ -4117,3 +4117,10 @@ TEST(HttpsToHttpRedirectTest3, SimpleInterface) {
   EXPECT_EQ(200, res->status);
 }
 #endif
+
+GTEST_API_ int main(int argc, char **argv) {
+  std::cout << "Running main() from gtest_main.cc\n";
+
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
